d844ac258f98c802f05329394ab18c39b8944984
Daniel <dan.r.french@sbcglobal.net>
2023-08-11 12:44:29 -0500

    first commit

---
+++ Airport_Playareas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Airport_Playareas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
